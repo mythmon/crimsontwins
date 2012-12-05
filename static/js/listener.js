f7949d4d3e8ebedb5511810c6ed37d4585fc3294
Mike Cooper <mythmon@gmail.com>
2012-12-05 10:50:22 -0800

    Pretty fade for images.

--- static/js/listener.js
+++ static/js/listener.js
@@ -1,9 +1,28 @@
 ;(function() {
 
+function updateContents($element) {
+  var $container = $('#container');
+  var $current = $container.children();
+  if (!$current.length) {
+    $container.html($element);
+    return;
+  }
+
+  $current.css({'opacity': 1});
+  $element.css({'opacity': 0});
+  $container.append($element);
+
+  $element.animate({opacity: 1}, {duration: 3000, queue: false});
+  $current.animate({opacity: 0}, {duration: 3000, queue: false, complete: function() {
+    console.log('done animating');
+    $current.remove();
+  }});
+}
+
 now.ready(function() {
   now.setUrl = function(url) {
     console.log('Loading url in iframe');
-    $('#container').html(
+    updateContents(
       $('<iframe>', {
         sandbox: 'allow-same-origin allow-scripts allow-forms',
         src: url
@@ -13,7 +32,7 @@ now.ready(function() {
 
   now.setImage = function(url) {
     console.log('Loading image');
-    $('#container').html(
+    updateContents(
       $('<div class="imgbg"/>').css({
         'background-image': 'url(' + url + ')'
       })
