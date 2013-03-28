d0c42659a819ccc1a60ddaccf9b6220bd403e2fc
Mike Cooper <mythmon@gmail.com>
2013-03-28 10:48:31 -0700

    Imgur modifier.

--- modifiers.js
+++ modifiers.js
@@ -10,14 +10,26 @@ exports.all.push(function blacklistNoodle(opts) {
   }
 });
 
-var youtube_re = RegExp('(youtube.com/watch\\?v=([A-Za-z0-9_]+))|(youtu.be/([A-Za-z0-9]+))');
+var youtube_re = RegExp('(youtube.com/.*\\?.*v=([A-Za-z0-9_-]+))|(youtu.be/([A-Za-z0-9_-]+))');
 exports.all.push(function embedYoutube(opts) {
-  var match = youtube_re.exec(opts.url);
+  var match = opts.url.match(youtube_re);
   if (match) {
     var id = match[2] || match[4];
     return {
       url: 'http://www.youtube.com/embed/' + id + '?rel=0&autoplay=1',
       type: 'url'
+    };
+  }
+});
+
+var imgur_re = RegExp('imgur.com/([A-Za-z0-9]+)');
+exports.all.push(function unpackImgur(opts) {
+  var match = opts.url.match(imgur_re);
+  if (match) {
+    var id = match[1];
+    return {
+      url: 'http://i.imgur.com/' + id + '.png', // The extension doesn't matter.
+      type: 'image'
     };
   }
 });
